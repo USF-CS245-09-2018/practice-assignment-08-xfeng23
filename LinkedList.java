8631e65dcfde6aeac7ca336c70ce9cabb31c5624
XueFeng <xfeng23@usfca.edu>
2018-11-02 13:33:33 -0700

    update.

---
+++ LinkedList.java
@@ -0,0 +1,34 @@
+
+public class LinkedList implements List {
+
+	@Override
+	public void add(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(int pos, Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get(int pos) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(int pos) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
