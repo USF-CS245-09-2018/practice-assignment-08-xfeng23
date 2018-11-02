a18e75c8612ad874c6407dc2210ce16839557e46
XueFeng <xfeng23@usfca.edu>
2018-11-02 15:42:41 -0700

    update.

--- LinkedList.java
+++ LinkedList.java
@@ -1,34 +1,88 @@
 
 public class LinkedList implements List {
 
+	public Node head;
+	public int size;
+
+	public LinkedList() {
+		size = 0;
+		head = null;
+	}
+
 	@Override
 	public void add(Object obj) throws Exception {
-		// TODO Auto-generated method stub
-		
+		Node newNode = new Node(obj);
+		newNode.next = head;
+		head = newNode;
+		size++;
+
 	}
 
 	@Override
 	public void add(int pos, Object obj) throws Exception {
-		// TODO Auto-generated method stub
-		
+		if (pos < 0 || pos > size) {
+			throw new IndexOutOfBoundsException("Index is out of the limits of the list");
+		}
+		if (pos == 0) {
+			Node newNode = new Node(obj);
+			newNode.next = head;
+			head = newNode;
+			size++;
+		} else {
+
+			Node previous = getNode(pos - 1);
+			Node newNode = new Node(obj);
+			newNode.next = previous.next;
+			previous.next = newNode;
+			size++;
+		}
+
 	}
 
 	@Override
-	public Object get(int pos) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Object get(int pos)throws Exception {
+	    Node node = getNode(pos);
+	    return node.getValue();
+	  }
+	
+	public Object getNode(int pos) {
+		if(pos<0||pos>size){
+		      throw new IndexOutOfBoundsException("Index is not in the list");
+		    }
+		      
+		   	Node current = head;
+				for(int i=0; i<pos; i++){
+		      current=current.next;
+		    }
+		    return current;
+
 	}
 
 	@Override
 	public Object remove(int pos) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos < 0 || pos > size) {
+			throw new IndexOutOfBoundsException("Index is out of the limits of the list");
+		}
+		if (pos == 0) {
+			Node current = head;
+			head = head.next;
+			size--;
+			return current.getValue();
+		} else {
+
+			Node previous = getNode(pos - 1);
+			Node current = getNode(pos);
+			Node temp = current;
+			current = current.next;
+			previous.next = current;
+			size--;
+			return temp.getValue();
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 }
